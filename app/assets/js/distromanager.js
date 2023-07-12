e4a2c3a6c8c4583fee23a902b71e7c24f5a00da1
ShEthan <48135622+ImShEthan@users.noreply.github.com>
2023-07-12 15:24:11 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://oricraft.fr/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://oricraft.fr/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
