bb6e7da76899e5a239f1135080cf7dc24e2ce95b
ImShEthan <ethan.alves@live.fr>
2023-11-21 01:09:34 +0100

    add new server

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://212.227.187.241/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://orignescraft.wstr.fr/files/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
